a06654c141e989563a97105e5de84b7db0eb8b98
IbrahimElmourchidi <ibrahimelmourchidi2@gmail.com>
2022-04-15 14:28:47 +0200

    docker task2

---
+++ docker/docker-compose/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const redis = require('redis');
+const client = redis.createClient({
+  host: 'redis-server',
+});
+
+client.set('visits', 0);
+app.all('', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send(`number of visits: ${visits}`);
+    client.set('visits', visits - -1);
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`listening on port 3000`);
+});
